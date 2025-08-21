67827e12b1a61e5d6a1b5db3af261aaa86c69217
Komal Verma <komalverma22012005@gmail.com>
2025-08-21 23:26:23 +0530

    created ping application

--- dist/index.js
+++ dist/index.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("hiii");
+Object.defineProperty(exports, "__esModule", { value: true });
+// console.log("let's start");
+const ws_1 = require("ws");
+const wss = new ws_1.WebSocketServer({ port: 8080 });
+// event handler
+wss.on("connection", function (socket) {
+    console.log("user connected");
+    // setInterval(()=>{
+    //     socket.send("current price of solana is "+Math.random())
+    // },500)
+    socket.on("message", (e) => {
+        // console.log(e.toString());
+        if (e.toString() === "ping") {
+            socket.send("pong");
+        }
+    });
+});
